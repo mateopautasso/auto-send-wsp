84e03b0195a1e1dcc9ee00ac6b466c2022e42e9d
mateopautasso <mateopautassodev@gmail.com>
2025-05-09 15:38:52 -0300

    refactor: :recycle: Refactor function names. Add new environment variables

--- index.ts
+++ index.ts
@@ -6,39 +6,44 @@ import path from 'path'
 
 dotenv.config()
 
-const MESSAGE = process.env.MESSAGE || ''
 const EXCEL_FILE_NAME = process.env.EXCEL_FILE_NAME || ''
 const EXCEL_PATH = path.resolve(__dirname, `${EXCEL_FILE_NAME}.xlsx`)
+const MESSAGE = process.env.MESSAGE || ''
+
+const SHORT_DELAY = process.env.SHORT_DELAY || '25'
+const SHORT_DELAY_RANGE_TO_MS = Number(SHORT_DELAY) * 1000
+
+const LONG_DELAY = process.env.LONG_DELAY || '30'
+const LONG_DELAY_TO_MS = Number(LONG_DELAY) * 60 * 1000
 
 function delay(ms: number) {
 	return new Promise((res) => setTimeout(res, ms))
 }
 
 function readNumbersFromExcel(): string[] {
-	const workSheetsFromFile = xlsx.parse(`${__dirname}/${EXCEL_FILE_NAME}.xlsx`)
+	const workSheetsFromFile = xlsx.parse(EXCEL_PATH)
 	const numbersArray = workSheetsFromFile[0].data.flat()
-	// const workbook = XLSX.readFile(EXCEL_PATH)
-	// const sheet = workbook.Sheets[workbook.SheetNames[0]]
-	// const data = XLSX.utils.sheet_to_json<{ numbers: string }>(sheet)
 	return numbersArray.map((number) => String(number))
 }
 
-async function sendMessages(client: Client, numeros: string[], mensaje: string) {
-	for (let i = 0; i < numeros.length; i++) {
-		const numeroFormateado = `${numeros[i]}@c.us`
+async function sendMessages(client: Client, numbers: string[], message: string) {
+	for (let i = 0; i < numbers.length; i++) {
+		const formattedNumber = `${numbers[i]}@c.us`
 
 		try {
-			await client.sendMessage(numeroFormateado, mensaje)
-			console.log(`✅ Mensaje enviado a: ${numeros[i]}`)
+			await client.sendMessage(formattedNumber, message)
+			console.log(`✅ Mensaje enviado a: ${numbers[i]}`)
 		} catch (err) {
-			console.error(`❌ Error al enviar a ${numeros[i]}:`, err)
+			console.error(`❌ Error al enviar a ${numbers[i]}:`, err)
 		}
 
 		if ((i + 1) % 50 === 0) {
-			console.log('⏸ Pausa de 30 minutos...')
-			await delay(30 * 60 * 1000)
+			console.log(`⏸ Pausa de ${LONG_DELAY} minutos...`)
+			await delay(LONG_DELAY_TO_MS)
 		} else {
-			const randomDelay = Math.floor(Math.random() * (35000 - 15000 + 1)) + 15000
+			const minRange = SHORT_DELAY_RANGE_TO_MS - 10000
+			const maxRange = SHORT_DELAY_RANGE_TO_MS + 10000
+			const randomDelay = Math.floor(Math.random() * (minRange - maxRange + 1)) + minRange
 			console.log(`⏱ Esperando ${randomDelay / 1000} segundos...`)
 			await delay(randomDelay)
 		}
@@ -46,7 +51,7 @@ async function sendMessages(client: Client, numeros: string[], mensaje: string) 
 }
 
 async function main() {
-	const numbers = readNumbersFromExcel()
+	const clientNumbers = readNumbersFromExcel()
 
 	const client = new Client({
 		authStrategy: new LocalAuth(),
@@ -63,7 +68,7 @@ async function main() {
 
 	client.on('ready', async () => {
 		console.log('✅ Cliente conectado. Iniciando envío de mensajes...')
-		await sendMessages(client, numbers, MESSAGE)
+		await sendMessages(client, clientNumbers, MESSAGE)
 		console.log('🏁 Envío completado.')
 		process.exit(0)
 	})
